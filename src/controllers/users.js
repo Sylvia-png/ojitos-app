0652fbf35886a36b73c41ef44f0765cb20b49fc6
sylvia <ojitoscecy.com>
2020-12-12 09:24:12 -0600

    update file

---
+++ src/controllers/users.js
@@ -0,0 +1,98 @@
+const mysql = require("mydb");
+const pool = require("../sql/connection");
+const { handleSQLError } = require("../sql/error");
+
+const getAllUsers = (req, res) => {
+  pool.query("SELECT * FROM users", (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+const getUserById = (req, res) => {
+  let sql = "SELECT * FROM users WHERE id = ?";
+  sql = mysql.format(sql, [req.params.id]);
+
+  pool.query(sql, (err, rows) => {
+    if (err) return handleSQLError(res, err);
+    return res.json(rows);
+  });
+};
+
+const login = (req, res) => {
+  const { username } = req.body;
+  let sql = "SELECT * FROM users WHERE username = ?";
+  sql = mysql.format(sql, [username]);
+
+  pool.query(sql, (err, res) => {
+    if (err) return handleSQLError(results, err);
+    return res.json({ message: `Login successful ${results.username}` });
+  });
+};
+
+const createUser = (req, res) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    username,
+    password,
+  } = req.body;
+  let sql =
+    "INSERT INTO users (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  sql = mysql.format(sql, [
+    first_name,
+    last_name,
+    email,
+    username,
+    password,
+  ]);
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err);
+    return res.json({ newId: results.insertId });
+  });
+};
+
+const updateUserById = (req, res) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+  } = req.body;
+  let sql =
+    "UPDATE users SET first_name = ?, last_name = ?, email = ?, username = ?, password = ? WHERE id = ?";
+  sql = mysql.format(sql, [
+    firstName,
+    lastName,
+    email,
+    username,
+    password,
+    req.params.id,
+  ]);
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err);
+    return res.status(204).json();
+  });
+};
+
+const deleteUserByUsername = (req, res) => {
+  let sql = "DELETE FROM users WHERE username = ?";
+  sql = mysql.format(sql, [req.params.username]);
+
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err);
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
+  });
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  login,
+  createUser,
+  updateUserById,
+  deleteUserByUsername,
+};
